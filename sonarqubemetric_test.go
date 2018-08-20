4660b2f8b8ccff7a975213de41b1eb69e91cdc6f
Pedro Enrique <pedro.tma@gmail.com>
2018-08-20 10:47:25 +0200

    Added Sonarqube metric struct

---
+++ sonarqubemetric_test.go
@@ -0,0 +1,283 @@
+// GENERATED CODE. DO NOT EDIT
+
+package schema
+
+import (
+	"context"
+	"testing"
+
+	"github.com/jhaynie/go-gator/orm"
+)
+
+func TestCreateSonarqubeMetricTable(t *testing.T) {
+	tx, err := GetDatabase().Begin()
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = DBCreateSonarqubeMetricTableTx(context.Background(), tx)
+	if err != nil {
+		tx.Rollback()
+		t.Fatal(err)
+	}
+	err = tx.Commit()
+	if err != nil {
+		tx.Rollback()
+		t.Fatal(err)
+	}
+}
+
+func TestCreateSonarqubeMetricDelete(t *testing.T) {
+	r := &SonarqubeMetric{
+		ID:                "a325dd42de49f7d3",
+		Checksum:          nil,
+		CustomerID:        "369a7cbda736b1c8",
+		ComponentIdExtID:  "0367c5a6ab97dbd3",
+		ComponentIdID:     "356939cbb1e4d3af",
+		ComponentKeyExtID: "415da04fcf09c163",
+		ExtID:             "f4fd463b29266bb9",
+		Date:              int64(64),
+		Metric:            "616ab7ec65bb0e35",
+		Value:             "dfe5ab1b943ebdc3",
+	}
+	ctx := context.Background()
+	db := GetDatabase()
+	DeleteAllSonarqubeMetrics(ctx, db)
+	result, err := r.DBCreate(ctx, db)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if result == nil {
+		t.Fatal("expected result to be non-nil")
+	}
+	count, err := r.DBCount(ctx, db)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if count != 1 {
+		t.Fatalf("count should have been 1 but was %d", count)
+	}
+	exists, err := r.DBExists(ctx, db)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !exists {
+		t.Fatal("exists should have been true but was false")
+	}
+	found, err := FindSonarqubeMetricByID(ctx, db, r.ID)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if found == nil {
+		t.Fatal("expected found to be a value but was nil")
+	}
+	if found.ID != r.ID {
+		t.Fatalf("expected found primary key to be %v but was %v", r.ID, found.ID)
+	}
+	if orm.Stringify(r) != orm.Stringify(found) {
+		t.Fatalf("expected r to be found but was different")
+	}
+	results, err := FindSonarqubeMetrics(ctx, db)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if results == nil {
+		t.Fatal("expected results to be a value but was nil")
+	}
+	if len(results) != 1 {
+		t.Log(orm.Stringify(results))
+		t.Fatalf("expected results length to be 1 but was %d", len(results))
+	}
+	f, err := r.DBFindOne(ctx, db, r.GetID())
+	if err != nil {
+		t.Fatal(err)
+	}
+	if f == false {
+		t.Fatal("expected found to be a true but was false")
+	}
+	a, b, err := r.DBUpsert(ctx, db)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if a {
+		t.Fatal("expected a to be false but was true")
+	}
+	if b {
+		t.Fatal("expected b to be false but was true")
+	}
+	r.SetCustomerID("a7dea500cb298bcf")
+
+	r.SetComponentIdExtID("467d30f5717404fd")
+
+	r.SetComponentIdID("2331679fab6dcf46")
+
+	r.SetComponentKeyExtID("a25b92a88c467439")
+
+	r.SetExtID("d4f5283cae8bbd39")
+
+	r.SetDate(int64(640))
+
+	r.SetMetric("ac4dcf587a850b23")
+
+	r.SetValue("6808f040c040e833")
+
+	a, b, err = r.DBUpsert(ctx, db)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !a {
+		t.Fatal("expected a to be true but was false")
+	}
+	if b {
+		t.Fatal("expected b to be false but was true")
+	}
+	_, err = r.DBDelete(ctx, db)
+	if err != nil {
+		t.Fatal(err)
+	}
+	count, err = r.DBCount(ctx, db)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if count != 0 {
+		t.Fatalf("count should have been 0 but was %d", count)
+	}
+	exists, err = r.DBExists(ctx, db)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if exists {
+		t.Fatal("exists should have been false but was true")
+	}
+}
+
+func TestCreateSonarqubeMetricDeleteTx(t *testing.T) {
+	r := &SonarqubeMetric{
+		ID:                "a325dd42de49f7d3",
+		Checksum:          nil,
+		CustomerID:        "369a7cbda736b1c8",
+		ComponentIdExtID:  "0367c5a6ab97dbd3",
+		ComponentIdID:     "356939cbb1e4d3af",
+		ComponentKeyExtID: "415da04fcf09c163",
+		ExtID:             "f4fd463b29266bb9",
+		Date:              int64(64),
+		Metric:            "616ab7ec65bb0e35",
+		Value:             "dfe5ab1b943ebdc3",
+	}
+	ctx := context.Background()
+	db := GetDatabase()
+	DeleteAllSonarqubeMetrics(ctx, db)
+	tx, err := db.Begin()
+	if err != nil {
+		t.Fatal(err)
+	}
+	result, err := r.DBCreateTx(ctx, tx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if result == nil {
+		t.Fatal("expected result to be non-nil")
+	}
+	count, err := r.DBCountTx(ctx, tx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if count != 1 {
+		t.Fatalf("count should have been 1 but was %d", count)
+	}
+	exists, err := r.DBExistsTx(ctx, tx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !exists {
+		t.Fatal("exists should have been true but was false")
+	}
+	found, err := FindSonarqubeMetricByIDTx(ctx, tx, r.ID)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if found == nil {
+		t.Fatal("expected found to be a value but was nil")
+	}
+	if found.ID != r.ID {
+		t.Fatalf("expected found primary key to be %v but was %v", r.ID, found.ID)
+	}
+	if orm.Stringify(r) != orm.Stringify(found) {
+		t.Fatalf("expected r to be found but was different")
+	}
+	results, err := FindSonarqubeMetricsTx(ctx, tx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if results == nil {
+		t.Fatal("expected results to be a value but was nil")
+	}
+	if len(results) != 1 {
+		t.Log(orm.Stringify(results))
+		t.Fatalf("expected results length to be 1 but was %d", len(results))
+	}
+	f, err := r.DBFindOneTx(ctx, tx, r.GetID())
+	if err != nil {
+		t.Fatal(err)
+	}
+	if f == false {
+		t.Fatal("expected found to be a true but was false")
+	}
+	a, b, err := r.DBUpsertTx(ctx, tx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if a {
+		t.Fatal("expected a to be false but was true")
+	}
+	if b {
+		t.Fatal("expected b to be false but was true")
+	}
+	r.SetCustomerID("a7dea500cb298bcf")
+
+	r.SetComponentIdExtID("467d30f5717404fd")
+
+	r.SetComponentIdID("2331679fab6dcf46")
+
+	r.SetComponentKeyExtID("a25b92a88c467439")
+
+	r.SetExtID("d4f5283cae8bbd39")
+
+	r.SetDate(int64(640))
+
+	r.SetMetric("ac4dcf587a850b23")
+
+	r.SetValue("6808f040c040e833")
+
+	a, b, err = r.DBUpsertTx(ctx, tx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !a {
+		t.Fatal("expected a to be true but was false")
+	}
+	if b {
+		t.Fatal("expected b to be false but was true")
+	}
+	_, err = r.DBDeleteTx(ctx, tx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	count, err = r.DBCountTx(ctx, tx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if count != 0 {
+		t.Fatalf("count should have been 0 but was %d", count)
+	}
+	exists, err = r.DBExistsTx(ctx, tx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if exists {
+		t.Fatal("exists should have been false but was true")
+	}
+	err = tx.Commit()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
