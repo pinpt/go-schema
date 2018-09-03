9634aaf6367d1f7d0a480657630db2f2b0b4d616
Rommel Sanchez <rsanchez@pinpt.com>
2018-09-03 15:08:02 -0500

    BE-888 Added schema for commitsummary

---
+++ commitsummary_test.go
@@ -0,0 +1,215 @@
+// GENERATED CODE. DO NOT EDIT
+
+package schema
+
+import (
+	"context"
+	"testing"
+
+	"github.com/jhaynie/go-gator/orm"
+)
+
+func TestCreateCommitSummaryTable(t *testing.T) {
+	tx, err := GetDatabase().Begin()
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = DBCreateCommitSummaryTableTx(context.Background(), tx)
+	if err != nil {
+		tx.Rollback()
+		t.Fatal(err)
+	}
+	err = tx.Commit()
+	if err != nil {
+		tx.Rollback()
+		t.Fatal(err)
+	}
+}
+
+func TestCreateCommitSummaryDelete(t *testing.T) {
+	r := &CommitSummary{
+		ID:           "f2304e5529d9ba57",
+		RepoID:       "d9d73c975aee36a9",
+		AuthorUserID: nil,
+		CustomerID:   "c9ad489ec64dacbe",
+		RefType:      "71badf00b022629e",
+		Additions:    int32(32),
+		Deletions:    int32(32),
+		FilesChanged: int32(32),
+		Branch:       nil,
+		Language:     "b0ca4ff49d5c17b5",
+		Date:         int64(64),
+		Message:      nil,
+	}
+	ctx := context.Background()
+	db := GetDatabase()
+	DeleteAllCommitSummaries(ctx, db)
+	result, err := r.DBCreate(ctx, db)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if result == nil {
+		t.Fatal("expected result to be non-nil")
+	}
+	count, err := r.DBCount(ctx, db)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if count != 1 {
+		t.Fatalf("count should have been 1 but was %d", count)
+	}
+	exists, err := r.DBExists(ctx, db)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !exists {
+		t.Fatal("exists should have been true but was false")
+	}
+	found, err := FindCommitSummaryByID(ctx, db, r.ID)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if found == nil {
+		t.Fatal("expected found to be a value but was nil")
+	}
+	if found.ID != r.ID {
+		t.Fatalf("expected found primary key to be %v but was %v", r.ID, found.ID)
+	}
+	if orm.Stringify(r) != orm.Stringify(found) {
+		t.Fatalf("expected r to be found but was different")
+	}
+	results, err := FindCommitSummaries(ctx, db)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if results == nil {
+		t.Fatal("expected results to be a value but was nil")
+	}
+	if len(results) != 1 {
+		t.Log(orm.Stringify(results))
+		t.Fatalf("expected results length to be 1 but was %d", len(results))
+	}
+	f, err := r.DBFindOne(ctx, db, r.GetID())
+	if err != nil {
+		t.Fatal(err)
+	}
+	if f == false {
+		t.Fatal("expected found to be a true but was false")
+	}
+	_, err = r.DBDelete(ctx, db)
+	if err != nil {
+		t.Fatal(err)
+	}
+	count, err = r.DBCount(ctx, db)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if count != 0 {
+		t.Fatalf("count should have been 0 but was %d", count)
+	}
+	exists, err = r.DBExists(ctx, db)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if exists {
+		t.Fatal("exists should have been false but was true")
+	}
+}
+
+func TestCreateCommitSummaryDeleteTx(t *testing.T) {
+	r := &CommitSummary{
+		ID:           "f2304e5529d9ba57",
+		RepoID:       "d9d73c975aee36a9",
+		AuthorUserID: nil,
+		CustomerID:   "c9ad489ec64dacbe",
+		RefType:      "71badf00b022629e",
+		Additions:    int32(32),
+		Deletions:    int32(32),
+		FilesChanged: int32(32),
+		Branch:       nil,
+		Language:     "b0ca4ff49d5c17b5",
+		Date:         int64(64),
+		Message:      nil,
+	}
+	ctx := context.Background()
+	db := GetDatabase()
+	DeleteAllCommitSummaries(ctx, db)
+	tx, err := db.Begin()
+	if err != nil {
+		t.Fatal(err)
+	}
+	result, err := r.DBCreateTx(ctx, tx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if result == nil {
+		t.Fatal("expected result to be non-nil")
+	}
+	count, err := r.DBCountTx(ctx, tx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if count != 1 {
+		t.Fatalf("count should have been 1 but was %d", count)
+	}
+	exists, err := r.DBExistsTx(ctx, tx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !exists {
+		t.Fatal("exists should have been true but was false")
+	}
+	found, err := FindCommitSummaryByIDTx(ctx, tx, r.ID)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if found == nil {
+		t.Fatal("expected found to be a value but was nil")
+	}
+	if found.ID != r.ID {
+		t.Fatalf("expected found primary key to be %v but was %v", r.ID, found.ID)
+	}
+	if orm.Stringify(r) != orm.Stringify(found) {
+		t.Fatalf("expected r to be found but was different")
+	}
+	results, err := FindCommitSummariesTx(ctx, tx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if results == nil {
+		t.Fatal("expected results to be a value but was nil")
+	}
+	if len(results) != 1 {
+		t.Log(orm.Stringify(results))
+		t.Fatalf("expected results length to be 1 but was %d", len(results))
+	}
+	f, err := r.DBFindOneTx(ctx, tx, r.GetID())
+	if err != nil {
+		t.Fatal(err)
+	}
+	if f == false {
+		t.Fatal("expected found to be a true but was false")
+	}
+	_, err = r.DBDeleteTx(ctx, tx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	count, err = r.DBCountTx(ctx, tx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if count != 0 {
+		t.Fatalf("count should have been 0 but was %d", count)
+	}
+	exists, err = r.DBExistsTx(ctx, tx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if exists {
+		t.Fatal("exists should have been false but was true")
+	}
+	err = tx.Commit()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
