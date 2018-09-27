4350d2466480a6c6aa838dbd13b95d8105d1ba34
Jeff Haynie <jhaynie@pinpt.com>
2018-09-26 20:38:44 -0500

    small changes to Makefile to make it easier to build and run. update with the latest routes from webapp

--- Makefile
+++ Makefile
@@ -6,12 +6,28 @@ ORG := pinpt
 PKG := $(ORG)/$(NAME)
 SHELL := /bin/bash
 
-.PHONY: all test
+.PHONY: all test dependencies build generate bindata schema clean
 
 all: test
 
 dependencies:
 	@dep ensure
+	@go get golang.org/x/tools/cmd/goimports
+
+build:
+	@go run main.go all
+
+generate:
+	@go run main.go generate
+
+bindata:
+	@go run main.go bindata
+
+schema:
+	@go run main.go schema
+
+clean:
+	@go run main.go clean
 
 test:
 	@go test -v ./... | grep -v "?"
--- migrations/20171108110099_rbac.sql
+++ migrations/20171108110099_rbac.sql
@@ -1,9 +1,9 @@
 -- +goose Up
 -- SQL in section 'Up' is executed when this migration is applied
 
-INSERT INTO `acl_role` (`id`,`name`,`description`,`created_at`) VALUES ("1f15a4c9d4798230","admin","Administrative role",1537990591593);
-INSERT INTO `acl_role` (`id`,`name`,`description`,`created_at`) VALUES ("3ae149a8dc444bce","executive","Executive role",1537990591593);
-INSERT INTO `acl_role` (`id`,`name`,`description`,`created_at`) VALUES ("455511f8e362deb9","manager","Manager role",1537990591593);
+INSERT INTO `acl_role` (`id`,`name`,`description`,`created_at`) VALUES ("1f15a4c9d4798230","admin","Administrative role",1538004640783);
+INSERT INTO `acl_role` (`id`,`name`,`description`,`created_at`) VALUES ("3ae149a8dc444bce","executive","Executive role",1538004640783);
+INSERT INTO `acl_role` (`id`,`name`,`description`,`created_at`) VALUES ("455511f8e362deb9","manager","Manager role",1538004640783);
 
 -- +goose Down
 -- SQL section 'Down' is executed when this migration is rolled back
