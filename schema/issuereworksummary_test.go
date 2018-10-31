46a1b079ff1a084c3a2009a58087bf80c3cdec0e
Rosalinda Macías Martínez <rmartinez@pinpt.com>
2018-10-31 13:11:14 -0600

    BE-1028: Add new table issue_rework_summary

---
+++ schema/issuereworksummary_test.go
@@ -0,0 +1,263 @@
+// GENERATED CODE. DO NOT EDIT
+
+package schema
+
+import (
+	"context"
+	"testing"
+
+	"github.com/jhaynie/go-gator/orm"
+)
+
+func TestCreateissueReworkSummaryTable(t *testing.T) {
+	tx, err := GetDatabase().Begin()
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = DBCreateissueReworkSummaryTableTx(context.Background(), tx)
+	if err != nil {
+		tx.Rollback()
+		t.Fatal(err)
+	}
+	err = tx.Commit()
+	if err != nil {
+		tx.Rollback()
+		t.Fatal(err)
+	}
+}
+
+func TestCreateissueReworkSummaryDelete(t *testing.T) {
+	r := &issueReworkSummary{
+		ID:         "e708e8dee1eb37ac",
+		Checksum:   nil,
+		CustomerID: "5f9ab367986997ee",
+		ProjectID:  "9490b1f1e18999f3",
+		UserID:     nil,
+		IssueID:    "31b59f8074e8ccb7",
+		Path:       "1264162994ce62b4",
+		Date:       nil,
+	}
+	ctx := context.Background()
+	db := GetDatabase()
+	DeleteAllIssueReworkSummaries(ctx, db)
+	result, err := r.DBCreate(ctx, db)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if result == nil {
+		t.Fatal("expected result to be non-nil")
+	}
+	count, err := r.DBCount(ctx, db)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if count != 1 {
+		t.Fatalf("count should have been 1 but was %d", count)
+	}
+	exists, err := r.DBExists(ctx, db)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !exists {
+		t.Fatal("exists should have been true but was false")
+	}
+	found, err := FindIssueReworkSummaryByID(ctx, db, r.ID)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if found == nil {
+		t.Fatal("expected found to be a value but was nil")
+	}
+	if found.ID != r.ID {
+		t.Fatalf("expected found primary key to be %v but was %v", r.ID, found.ID)
+	}
+	if orm.Stringify(r) != orm.Stringify(found) {
+		t.Fatalf("expected r to be found but was different")
+	}
+	results, err := FindIssueReworkSummaries(ctx, db)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if results == nil {
+		t.Fatal("expected results to be a value but was nil")
+	}
+	if len(results) != 1 {
+		t.Log(orm.Stringify(results))
+		t.Fatalf("expected results length to be 1 but was %d", len(results))
+	}
+	f, err := r.DBFindOne(ctx, db, r.GetID())
+	if err != nil {
+		t.Fatal(err)
+	}
+	if f == false {
+		t.Fatal("expected found to be a true but was false")
+	}
+	a, b, err := r.DBUpsert(ctx, db)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if a {
+		t.Fatal("expected a to be false but was true")
+	}
+	if b {
+		t.Fatal("expected b to be false but was true")
+	}
+	r.SetCustomerID("ec1ae0b48d505f1a")
+
+	r.SetProjectID("4f898c03de96b277")
+
+	r.SetIssueID("9abd787e4819cf1c")
+
+	r.SetPath("92887262804f5fd4")
+
+	a, b, err = r.DBUpsert(ctx, db)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !a {
+		t.Fatal("expected a to be true but was false")
+	}
+	if b {
+		t.Fatal("expected b to be false but was true")
+	}
+	_, err = r.DBDelete(ctx, db)
+	if err != nil {
+		t.Fatal(err)
+	}
+	count, err = r.DBCount(ctx, db)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if count != 0 {
+		t.Fatalf("count should have been 0 but was %d", count)
+	}
+	exists, err = r.DBExists(ctx, db)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if exists {
+		t.Fatal("exists should have been false but was true")
+	}
+}
+
+func TestCreateissueReworkSummaryDeleteTx(t *testing.T) {
+	r := &issueReworkSummary{
+		ID:         "e708e8dee1eb37ac",
+		Checksum:   nil,
+		CustomerID: "5f9ab367986997ee",
+		ProjectID:  "9490b1f1e18999f3",
+		UserID:     nil,
+		IssueID:    "31b59f8074e8ccb7",
+		Path:       "1264162994ce62b4",
+		Date:       nil,
+	}
+	ctx := context.Background()
+	db := GetDatabase()
+	DeleteAllIssueReworkSummaries(ctx, db)
+	tx, err := db.Begin()
+	if err != nil {
+		t.Fatal(err)
+	}
+	result, err := r.DBCreateTx(ctx, tx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if result == nil {
+		t.Fatal("expected result to be non-nil")
+	}
+	count, err := r.DBCountTx(ctx, tx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if count != 1 {
+		t.Fatalf("count should have been 1 but was %d", count)
+	}
+	exists, err := r.DBExistsTx(ctx, tx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !exists {
+		t.Fatal("exists should have been true but was false")
+	}
+	found, err := FindIssueReworkSummaryByIDTx(ctx, tx, r.ID)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if found == nil {
+		t.Fatal("expected found to be a value but was nil")
+	}
+	if found.ID != r.ID {
+		t.Fatalf("expected found primary key to be %v but was %v", r.ID, found.ID)
+	}
+	if orm.Stringify(r) != orm.Stringify(found) {
+		t.Fatalf("expected r to be found but was different")
+	}
+	results, err := FindIssueReworkSummariesTx(ctx, tx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if results == nil {
+		t.Fatal("expected results to be a value but was nil")
+	}
+	if len(results) != 1 {
+		t.Log(orm.Stringify(results))
+		t.Fatalf("expected results length to be 1 but was %d", len(results))
+	}
+	f, err := r.DBFindOneTx(ctx, tx, r.GetID())
+	if err != nil {
+		t.Fatal(err)
+	}
+	if f == false {
+		t.Fatal("expected found to be a true but was false")
+	}
+	a, b, err := r.DBUpsertTx(ctx, tx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if a {
+		t.Fatal("expected a to be false but was true")
+	}
+	if b {
+		t.Fatal("expected b to be false but was true")
+	}
+	r.SetCustomerID("ec1ae0b48d505f1a")
+
+	r.SetProjectID("4f898c03de96b277")
+
+	r.SetIssueID("9abd787e4819cf1c")
+
+	r.SetPath("92887262804f5fd4")
+
+	a, b, err = r.DBUpsertTx(ctx, tx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !a {
+		t.Fatal("expected a to be true but was false")
+	}
+	if b {
+		t.Fatal("expected b to be false but was true")
+	}
+	_, err = r.DBDeleteTx(ctx, tx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	count, err = r.DBCountTx(ctx, tx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if count != 0 {
+		t.Fatalf("count should have been 0 but was %d", count)
+	}
+	exists, err = r.DBExistsTx(ctx, tx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if exists {
+		t.Fatal("exists should have been false but was true")
+	}
+	err = tx.Commit()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
